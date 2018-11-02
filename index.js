db6c10700234acf1fd552e9155150b61837b5dfd
Luis Zabala <luis.zabala076@gmail.com>
2018-11-02 22:28:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function happyHolidays () {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
+function happyHolidayTo(name) {
+  return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday} , ${name}!`;
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday} , ${name}!`
 }
 
 function holidayCountdown(xmas, num) {
-  return `It's ${20} days until ${xmas}!`;
+  return `It's ${20} days until ${xmas}!`
 }
